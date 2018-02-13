002d486d2e3156b0e25f71809aa982506df3db36
KOUTILYA GOWTHAM SURYADEVARA <honey.gouti@gmail.com>
2018-02-13 14:19:24 +0000

    Create variables.tf
    Added variables that are useful to spinup vpc

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {}
+variable "aws_sceret_key" {}
+variable "aws_key_path" {}
+variable "aws_key_name" {}
+
+variable "aws_nat_ami" {
+             default = "ami-2e1bc047"
+}
+
+variable "aws_ubuntu_ami" {
+             default = "ami-b227efda"
+}
