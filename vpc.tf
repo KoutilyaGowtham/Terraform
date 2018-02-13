1be62bc8bc2694381131e2322ed2ad703f3d57ee
KOUTILYA GOWTHAM SURYADEVARA <honey.gouti@gmail.com>
2018-02-13 16:47:11 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,16 +1,16 @@
 provider "aws" {
         access_key = "${var.aws_access_key}"
         secret_key = "${var.aws_secret_key}"
-        region = "us-east-1"
+        region = "${var.region}"
         
 }
 
 resource "aws_vpc" "default" {
-        cidr_block = "10.0.0.0/16"
+        cidr_block = "${var.cidr_block}"
         
 }
 
-resource "aws_internet_gateway" {
+resource "aws_internet_gateway" "default" {
         vpc_id = "${aws_vpc.default.id}"
         
 }
@@ -36,7 +36,7 @@ resource "aws_security_group" "nat" {
 resource "aws_instance" "nat" {
         ami = "${var.aws_ami_nat}"
         instance_type = "${var.aws_instance_type}"
-        availability_zone = "${var.aws_availabilty_zone}"
+        availability_zone = "us-east-1b"
         key_name = "${var.aws_key_name}"
         security_groups = "${aws_security_group.nat.id}"
         subnet_id = "${aws_subnet.us-east-1b-public.id}"
@@ -47,4 +47,25 @@ resource "aws_instance" "nat" {
 resource "aws_eip" "nat" {
         instance_id = "${aws_intance.nat.id}"
         vpc = true
+}
+
+resource "aws_route_table" "public_route_table" {
+        vpc_id = "${aws_vpc.default.id}"
+        route {
+                cidr_block = "0.0.0.0/16"
+                gateway_id = "${aws_internet_gateway.default.id}"
+                }
+        }
+
+# Creating the public subnets
+resource "aws_subnets" "public-1b" {
+        vpc_id = "${aws_vpc.default.id}"
+        cidr_block = "10.0.0.0/24"
+        availability_zone = "us-east-1b"
+        }
+
+resource "aws_subnet" "public-1d" {
+        vpc_id = "${aws_vpc.default.id}"
+        cidr_block = "10.0.2.0/24"
+        availability_zone = "us-east-1d"
         }
