4262e0d034db31eaa5dea756921444e3e0ee51e3
KOUTILYA GOWTHAM SURYADEVARA <honey.gouti@gmail.com>
2018-02-13 14:39:41 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -4,4 +4,31 @@ provider "aws" {
         region = "us-east-1"
         
 }
+
+resource "aws_vpc" "default" {
+        cidr_block = "10.0.0.0/16"
         
+}
+
+resource "aws_internet_gateway" {
+        vpc_id = "${aws_vpc.default.id}"
+        
+}
+
+resource "aws_security_group" "nat" {
+        name = "nat"
+        description = "Allowing the internet access to private subnets through NAT Gateway"
+        
+        ingress {
+                from_port = 0
+                to_port = 65535
+                protocol ="tcp"
+                cidr_block = "${aws_subnet.us-east-1b-private.cidr_block}"
+                }
+        ingress {
+                from_port = 0
+                to_port = 65535
+                protocol = "tcp"
+                cidr_block = "${aws_subnet.us-east-1d-private.cidr_block}"
+                }
+        }
