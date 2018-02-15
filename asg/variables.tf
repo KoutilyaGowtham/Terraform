58ff1dbeb4199c9a11a85678f2ed79f4342f513d
KOUTILYA GOWTHAM SURYADEVARA <honey.gouti@gmail.com>
2018-02-15 09:24:58 -0500

    Update variables.tf

--- asg/variables.tf
+++ asg/variables.tf
@@ -1,3 +1,24 @@
+## Input variables for creating ASG
+
+# Assinging aws region to the ASG
 variable "region" {
   default = "us-east-1"
   }
+variable "stack_item_name" {
+  description = "Fullname of Stack item. The value is used to create the application resource tag"
+  type= "string"
+  }
+variable "stack_item_shortname" {
+  description = "This is the short form of the tag"
+  type = "string"
+  }
+variable "asg_name_override" {
+  type = "string"
+  description = " To override the ASG name"
+  default = ""
+  }
+variable "lc_sg_name_override" {
+  type = "string"
+  
+  
+
